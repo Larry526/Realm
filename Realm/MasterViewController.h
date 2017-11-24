7409f844ce2f3b5eec2ccbe7c28003e96841e15c
Larry Luk <larryluk526@gmail.com>
2017-11-24 14:01:55 -0500

    Initial Commit

---
+++ Realm/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  Realm
+//
+//  Created by Larry Luk on 2017-11-24.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
